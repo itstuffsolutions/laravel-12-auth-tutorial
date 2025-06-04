3d3be1d97e869bbab8615b6d6f87b88f60cba365
soni <sonimaurya1224@gmail.com>
2025-06-04 10:55:28 +0530

    second commit

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -17,7 +17,11 @@ class ImageController extends Controller
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048 |dimensions:min_width=100,max_width=500',
         ]);
         $imageName = time(). '_' . Str::random(10).'.'.$request->image->extension();
+        //store file in public folder
         $request->image->move(public_path('uploads'), $imageName);
+       //store file in storage folder
+       //$path = $request->file('image')->storeAs($imageName);
+       
 
         return back()->with('success', 'Image uploaded successfully!');
     }
